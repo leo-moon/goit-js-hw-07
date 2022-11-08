2e182ba6b250ab08214e5345191ac5f31c88c33b
Vladimir Larin <lvv0820@gmail.com>
2022-11-08 13:00:18 +0200

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// const galleryCreate = (galleryItems) => {
+
+const galleryItem = galleryItems.map(({ preview, original, description }) => {
+  console.log(preview, original, description)
+  const divRef = document.createElement('div');
+  divRef.classList.add('.gallery__item');
+
+  const imgPreviewRef = document.createElement('img');
+  imgPreviewRef.classList.add('.gallery__image');
+  imgPreviewRef.url = preview;
+  imgPreviewRef.alt = 'previewPhoto';
+
+  const imgMainRef = document.createElement('img');
+  imgMainRef.classList.add('.gallery__image');
+  imgMainRef.url = original;
+  imgMainRef.alt = 'originalPhoto';
+
+  
+});
+console.log(galleryItem);
+
+
+// export const galleryItems = [
+//     preview:
+//     original:
+//     description:
+
+// .gallery 
+// 
+// .gallery__image:hover
+//  
+// .gallery__link
+// .gallery__link:hover 
+
