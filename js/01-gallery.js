1661bd2ca55da8736cd9dddee793cab8fb5e8eb2
Vladimir Larin <lvv0820@gmail.com>
2022-11-08 13:18:10 +0200

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,21 +8,34 @@ console.log(galleryItems);
 const galleryItem = galleryItems.map(({ preview, original, description }) => {
   console.log(preview, original, description)
   const divRef = document.createElement('div');
-  divRef.classList.add('.gallery__item');
+  divRef.classList.add('gallery__item');
+  console.log(divRef);
+
+  const aRef = document.createElement('a');
+  aRef.classList.add('gallery__link');
+  aRef.href = original;
+  aRef.alt = 'originalPhoto';
 
   const imgPreviewRef = document.createElement('img');
-  imgPreviewRef.classList.add('.gallery__image');
-  imgPreviewRef.url = preview;
-  imgPreviewRef.alt = 'previewPhoto';
+  imgPreviewRef.classList.add('gallery__image');
+  imgPreviewRef.src = preview;
+  imgPreviewRef.dataset.source = original;
+  imgPreviewRef.alt = description;
 
-  const imgMainRef = document.createElement('img');
-  imgMainRef.classList.add('.gallery__image');
-  imgMainRef.url = original;
-  imgMainRef.alt = 'originalPhoto';
 
-  
+
 });
 console.log(galleryItem);
+
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description" */
+
 
 
 // export const galleryItems = [
